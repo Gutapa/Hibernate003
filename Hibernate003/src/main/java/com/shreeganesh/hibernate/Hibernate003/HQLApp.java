12b7e277cb9fdb5c51f67e6f31c8e228e8c80f3c
Krishna Gupta <krishnagupta24697@gmail.com>
2022-09-22 19:27:17 +0530

    Added simple HQL queries to view data from database by creating query

--- Hibernate003/src/main/java/com/shreeganesh/hibernate/Hibernate003/HQLApp.java
+++ Hibernate003/src/main/java/com/shreeganesh/hibernate/Hibernate003/HQLApp.java
@@ -1,30 +1,50 @@
 package com.shreeganesh.hibernate.Hibernate003;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
-import com.shreeganesh.hibernate.Hibernate003.entity.Address;
 import com.shreeganesh.hibernate.Hibernate003.entity.Student;
 import com.shreeganesh.hibernate.Hibernate003.utils.HibernateUtils;
 
 public class HQLApp {
+	
+	static Session session=null;
+	
 	public static void main(String[] args) {
 		SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
 
-		Session session = sessionFactory.openSession();
+		session = sessionFactory.openSession();
 		
 		Transaction transaction = session.getTransaction();
 		
-		
 		transaction.begin();
 		
-		Student student = session.get(Student.class, 1);
 		
-		System.out.println(student);
+		getAllStudents();
+		
+		getStudentsByAge(24);
 		
 		transaction.commit();
 
 		session.close();
+	}
+
+	private static void getStudentsByAge(int age) {
+		Query<Student> query = session.createQuery("from Student s where s.age=?1", Student.class);
+		query.setParameter(1, age);
+		List<Student> list = query.list();
+		list.forEach(System.out::println);
+	}
+
+	private static void getAllStudents() {
+		//Query<Student> createQuery = session.createQuery("select s from Student s",Student.class);
+		Query<Student> createQuery = session.createQuery("from Student s",Student.class);
+		List<Student> list = createQuery.list();
+		list.forEach(System.out::println);
 	}
 }
