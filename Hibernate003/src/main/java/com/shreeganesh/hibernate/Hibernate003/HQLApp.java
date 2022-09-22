33669492dbd7a6e46e04b38738fb8d74e1f7b357
Krishna Gupta <krishnagupta24697@gmail.com>
2022-09-22 22:17:10 +0530

    Added various methods for select operation of DB using hQL Query

--- Hibernate003/src/main/java/com/shreeganesh/hibernate/Hibernate003/HQLApp.java
+++ Hibernate003/src/main/java/com/shreeganesh/hibernate/Hibernate003/HQLApp.java
@@ -24,14 +24,58 @@ public class HQLApp {
 		
 		transaction.begin();
 		
-		
+		System.out.println("******************************************************************************************************");
 		getAllStudents();
-		
+		System.out.println("******************************************************************************************************");
+		getNameOfAllStudents();
+		System.out.println("******************************************************************************************************");
+		getNameAgeOfAllStudents();
+		System.out.println("******************************************************************************************************");
 		getStudentsByAge(24);
+		System.out.println("******************************************************************************************************");
+		getStudentsByNameAge("Krishna",25);
+		System.out.println("******************************************************************************************************");
+		getStudentsBetweenAge(20,45);
+		System.out.println("******************************************************************************************************");
 		
 		transaction.commit();
 
 		session.close();
+	}
+
+	private static void getNameAgeOfAllStudents() {
+		Query<Object[]> query = session.createQuery("select s.name,s.age from Student s", Object[].class);
+		List<Object[]> list = query.list();
+		for(Object[] a: list) {
+			System.out.println(a[0]+" - "+a[1]);
+		}
+	}
+
+	private static void getNameOfAllStudents() {
+
+		Query<String> query = session.createQuery("select s.name from Student s",String.class);
+		List<String> list = query.list();
+		list.forEach(System.out::println);
+	}
+
+	private static void getStudentsBetweenAge(int min_age, int max_age) {
+
+		Query<Student> query = session.createQuery("from Student s where s.age between :MIN_AGE and :MAX_AGE", Student.class);
+		query.setParameter("MIN_AGE", min_age);
+		query.setParameter("MAX_AGE", max_age);
+		List<Student> list = query.list();
+		list.forEach(System.out::println);
+		
+	}
+
+	private static void getStudentsByNameAge(String name, int age) {
+		
+		Query<Student> query = session.createQuery("from Student s where s.age=:AGE and s.name=:NAME", Student.class);
+		query.setParameter("AGE", age);
+		query.setParameter("NAME", name);
+		List<Student> list = query.list();
+		list.forEach(System.out::println);
+		
 	}
 
 	private static void getStudentsByAge(int age) {
